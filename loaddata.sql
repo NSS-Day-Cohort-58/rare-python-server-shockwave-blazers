25eeebb2481494e544c57869db91c6a97c4baa1a
Jonathan Woodard <jvw.226@gmail.com>
2022-10-19 10:35:11 -0500

    added data to SQL table for users

--- loaddata.sql
+++ loaddata.sql
@@ -89,3 +89,4 @@ INSERT INTO Categories ('label') VALUES ('News');
 INSERT INTO Tags ('label') VALUES ('JavaScript');
 INSERT INTO Reactions ('label', 'image_url') VALUES ('happy', 'https://pngtree.com/so/happy');
 INSERT INTO `Users` VALUES (null, 'Brandon', 'Herbison', 'herbisonbrandon@gmail.com', 'I am badass', 'brandonherbison', 'adamsux', null, null, null);
+
