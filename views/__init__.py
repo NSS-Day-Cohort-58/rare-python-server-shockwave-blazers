01f12a65dcb486974860caf0b207e8d3703d8c81
Shane Standifur <shanestandifur@gmail.com>
2022-10-19 14:42:42 -0500

    added create subscriptions functions

--- views/__init__.py
+++ views/__init__.py
@@ -1,6 +1,6 @@
 
 from .post_requests import get_all_posts, get_single_post, create_post, update_post, delete_post
 from .reaction_requests import get_all_reactions
-from .subscriptions_requests import get_all_subscriptions, get_single_subscription, delete_subscription, update_subscription
+from .subscriptions_requests import get_all_subscriptions, get_single_subscription, delete_subscription, update_subscription,create_subscription
 from .categories_requests import get_all_categories, get_single_category, update_category, delete_category,create_category
 
--- views/subscriptions_requests.py
+++ views/subscriptions_requests.py
@@ -92,3 +92,28 @@ def update_subscription(id, new_subscription):
         return False
     else:
         return True
+
+def create_subscription(new_subscription):
+    with sqlite3.connect("./rare.sqlite3") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        INSERT INTO Subscription
+            ( follower_id, author_id, created_on )
+        VALUES
+            ( ?, ?, ?, ?, ?);
+        """, (new_subscription['follower_id'], new_subscription['author_id'],
+              new_subscription['created_on'], ))
+
+        # The `lastrowid` property on the cursor will return
+        # the primary key of the last thing that got added to
+        # the database.
+        id = db_cursor.lastrowid
+
+        # Add the `id` property to the animal dictionary that
+        # was sent by the client so that the client sees the
+        # primary key in the response.
+        new_subscription['id'] = id
+
+
+    return new_subscription
