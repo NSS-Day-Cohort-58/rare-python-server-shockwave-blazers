db1f68eda8ffcbd21fbdfe66dd6dcc3e03250b53
Jonathan Woodard <jvw.226@gmail.com>
2022-10-19 11:39:18 -0500

    created class initializers for subscriptions and comments

---
+++ models/comment.py
@@ -0,0 +1,10 @@
+class Comment():
+
+    # Class Initializer, takes 5 parameters
+    def __init__(self, id, author_id, post_id, content):
+        self.id = id
+        self.author_id = author_id
+        self.post_id = post_id
+        self.content = content
+        self.author_id = None
+        self.post_id = None
